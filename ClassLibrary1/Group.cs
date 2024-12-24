5caba5e2a28e86cb8bc547ed4ac85512aa2e3ddf
usupova <157115365+usupova@users.noreply.github.com>
2024-12-24 12:06:39 +0300

    Add files via upload

---
+++ ClassLibrary1/Group.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using static ClassLibrary1.Group;
+
+namespace ClassLibrary1
+{
+    public class Group
+    {
+
+      public  int Name { get; set; }
+      public string College { get; set; }
+        public List<Student> Students { get; set; }
+
+
+        public Group (int name, string college)
+        {
+            Name = name;
+            College = college;
+            Students = new List <Student> ();
+        
+        }
+        public void addStudent(Student  student)
+        {
+            Students.Add (student);
+        }
+        public void show()
+        {
+            Console.WriteLine($"Название: {Name}, Колледж: {College}");
+        }
+
+
+        public void ShowAll()
+        {
+            foreach (var student in Students)
+            {
+                student.show();
+      
+            }        
+        }
+    }
+}
---
+++ ClassLibrary1/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ClassLibrary1
+{
+    public class Student
+    {
+        public string Nam { get; set; }
+        public int Ocenka { get; set; }
+        public string Name { get; set; }
+
+
+        public Student(string nam, int ocenka,string name)
+        { 
+            Nam = nam;
+            Ocenka = ocenka;
+            Name = name;
+        }
+        public void show()
+        {
+            Console.WriteLine($"Название: {Nam},Фамилия {Name}  Группа: {Ocenka}");
+        }
+    }
+}
